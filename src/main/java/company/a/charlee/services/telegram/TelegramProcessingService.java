07d42fb967e415f48153f3474dfe3b88c9c3d2e2
ostas <ostashkozhenya@gmail.com>
2024-04-15 22:06:13 +0300

    fix application context

--- src/main/java/company/a/charlee/services/telegram/TelegramProcessingService.java
+++ src/main/java/company/a/charlee/services/telegram/TelegramProcessingService.java
@@ -22,6 +22,7 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
